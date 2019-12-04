9284f3c6eccea7322aa6b4ddfc55e46ebf691d67
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-12-05 00:35:42 +0400

    Добавляет стили блока modal

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,48 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var buttonCall = document.querySelector('.btn--call');
+var popupForm = document.querySelector('.modal-form');
+var popupClose = popupForm.querySelector('.modal-form__btn-close');
+var form = popupForm.querySelector('form');
+var nameInput = popupForm.querySelector('[name=name]');
+var phoneInput = popupForm.querySelector('[name=phone]');
+var messageInput = popupForm.querySelector('[name=question]');
+var overlay = document.querySelector('.modal__overlay');
 
-pageHeader.classList.remove('page-header--nojs');
+if (buttonCall && overlay) {
+  buttonCall.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    popupForm.classList.add('modal--show');
+    overlay.classList.add('modal__overlay--show')
+    nameInput.focus();
+  })
+}
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+if (popupClose) {
+  popupClose.addEventListener('click', function() {
+    popupForm.classList.remove('modal--show');
+    overlay.classList.remove('modal__overlay--show');
+  })
+}
+
+if (overlay) {
+  overlay.addEventListener('click', function() {
+    popupForm.classList.remove('modal--show');
+    overlay.classList.remove('modal__overlay--show');
+  })
+}
+
+window.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popupForm.classList.contains('modal--show')) {
+      popupForm.classList.remove('modal--show');
+      overlay.classList.remove('modal__overlay--show');
+    }
   }
-});
+})
+
+form.addEventListener("submit", function () {
+    localStorage.setItem("userName", nameInput.value);
+    localStorage.setItem("userPhone", phoneInput.value);
+    localStorage.setItem("userMessage", messageInput.value);
+  })
