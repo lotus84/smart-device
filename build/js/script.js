ba9bc435b799451e4b979329538186a94a3aec9d
Elena Mikirtumova <mikirtumova84@gmail.com>
2019-12-07 15:51:14 +0400

    Устраняет ошибки, возникающие при запуске npm test

--- build/js/script.js
+++ build/js/script.js
@@ -10,29 +10,29 @@ var overlay = document.querySelector('.modal__overlay');
 var toggles = document.querySelectorAll('.footer__toggle');
 
 if (buttonCall && overlay) {
-  buttonCall.addEventListener('click', function(evt) {
+  buttonCall.addEventListener('click', function (evt) {
     evt.preventDefault();
     popupForm.classList.add('modal--show');
-    overlay.classList.add('modal__overlay--show')
+    overlay.classList.add('modal__overlay--show');
     nameInput.focus();
-  })
+  });
 }
 
 if (popupClose) {
-  popupClose.addEventListener('click', function() {
+  popupClose.addEventListener('click', function () {
     popupForm.classList.remove('modal--show');
     overlay.classList.remove('modal__overlay--show');
-  })
+  });
 }
 
 if (overlay) {
-  overlay.addEventListener('click', function() {
+  overlay.addEventListener('click', function () {
     popupForm.classList.remove('modal--show');
     overlay.classList.remove('modal__overlay--show');
-  })
+  });
 }
 
-window.addEventListener('keydown', function(evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (popupForm.classList.contains('modal--show')) {
@@ -40,24 +40,24 @@ window.addEventListener('keydown', function(evt) {
       overlay.classList.remove('modal__overlay--show');
     }
   }
-})
+});
 
-form.addEventListener("submit", function () {
-    localStorage.setItem("userName", nameInput.value);
-    localStorage.setItem("userPhone", phoneInput.value);
-    localStorage.setItem("userMessage", messageInput.value);
-  })
+form.addEventListener('submit', function () {
+  localStorage.setItem('userName', nameInput.value);
+  localStorage.setItem('userMessage', messageInput.value);
+  localStorage.setItem('userPhone', phoneInput.value);
+});
 
-  if (toggles) {
-    for (var i = 0; i < toggles.length; i++) {
-      toggles[i].addEventListener('click', function() {
-        this.classList.toggle('.footer__toggle--active');
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-      })
-    }
+if (toggles) {
+  for (var i = 0; i < toggles.length; i++) {
+    toggles[i].addEventListener('click', function () {
+      this.classList.toggle('.footer__toggle--active');
+      var panel = this.nextElementSibling;
+      if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+      } else {
+        panel.style.display = 'block';
+      }
+    });
   }
+}
